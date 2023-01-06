f9db87725e9c05df14f482104a7321ef5fb8f447
Sui <suihsilan@gmail.com>
2023-01-06 15:15:01 +1100

    add comment and note

--- week2/login.js
+++ week2/login.js
@@ -1,9 +1,13 @@
+//載入vue esm的cdn
 import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 
+//起手式，建立Vue元件
 const app = {
   data() {
+    //資料(函式)
     return {
       user: {
+        //使用者登入的帳密
         username: "",
         password: "",
       },
@@ -11,10 +15,15 @@ const app = {
   },
   methods: {
     login() {
+      //登入方法,我們會透過v-on:click綁定login方法，在使用者在畫面上點擊登入，觸發login行為
+      //登入的api網址
       const api = "https://vue3-course-api.hexschool.io/v2/admin/signin";
+      //發送登入請求
       axios
-        .post(api, this.user)
+        .post(api, this.user) //this會將user的內容展開在Proxy的target物件下，當值改變時，Proxy會去攔截這裡面的值也作出相應的變化(我們透過v-model綁定input-username以及input-password,當使用者在畫面的input中輸入內容，因此也同樣會從這裡傳遞給伺服器去資料庫做比對是否有這個使用者)
         .then((response) => {
+          //成功回應
+          //需要取的token和expired並且存到cookie中
           const { token, expired } = response.data;
           // 把expired token寫入 cookie
           // expires 設置有效時間
@@ -23,10 +32,11 @@ const app = {
           //夾帶token在header中
           //如何夾在token在header中透過axios傳給伺服器
           axios.defaults.headers.common["Authorization"] = token;
-          //跳轉頁面
-          window.location = "products.html";
+          //因為登入請求進入then代表登入成功，會跳轉頁面至產品列表
+          window.location = "./products.html";
         })
         .catch((err) => {
+          //錯誤，清空欄位，跳出錯誤訊息重新輸入
           this.user.username = "";
           this.user.password = "";
           alert(err.response.data.message);
@@ -34,5 +44,5 @@ const app = {
     },
   },
 };
-
+//生成Vue元件的資料，並傳遞到畫面上
 createApp(app).mount("#app");
--- week2/products.js
+++ week2/products.js
@@ -3,22 +3,26 @@ import { createApp } from "https://unpkg.com/vue@3/dist/vue.esm-browser.js";
 createApp({
   data() {
     return {
+      //固定網址
       apiUrl: "https://vue3-course-api.hexschool.io/v2",
-      apiPath: "sui-vue",
-      products: [],
-      tempProduct: {},
+      apiPath: "sui-vue", //api_path
+      products: [], //資料列表變數
+      tempProduct: {}, //單筆資料變數
     };
   },
   methods: {
+    //驗證方法
     checkAdmin() {
       const url = `${this.apiUrl}/api/user/check`;
       axios
         .post(url)
         .then(() => {
+          //調用getData()方法 取的產品清單
           this.getData();
         })
         .catch((err) => {
           alert(err.response.data.message);
+          //錯誤跳轉回登入頁面
           window.location = "login.html";
         });
     },
@@ -28,24 +32,27 @@ createApp({
         .get(url)
         .then((response) => {
           console.log(response.data.products);
+          //把我在遠端的產品列表資料傳入到this.produts裡
           this.products = response.data.products;
         })
         .catch((err) => {
           alert(err.response.data.message);
         });
     },
-    openProduct(item) {
-      this.tempProduct = item;
+    openProduct(product) {
+      this.tempProduct = product;
     },
   },
+  //生命週期，初始化之後，立即調用，最常用的callback fn用來掛載到頁面上
   mounted() {
     // 取出 Token
     const token = document.cookie.replace(
       /(?:(?:^|.*;\s*)hexToken\s*=\s*([^;]*).*$)|^.*$/,
       "$1"
     );
+    //將token夾帶在header中axios請求送出給伺服器
     axios.defaults.headers.common["Authorization"] = token;
-
+    //觸法驗證方法
     this.checkAdmin();
   },
 }).mount("#app");
